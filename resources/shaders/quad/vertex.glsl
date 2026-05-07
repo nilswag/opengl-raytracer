4c7fc3f3ff54a8495c256bb702d21e0fe2a8b792
nilswag <nilswagemaker05@icloud.com>
2026-05-07 16:39:03 +0200

    Restart #10000, fuck you daan

--- resources/shaders/quad/vertex.glsl
+++
@@ -1,9 +0,0 @@
-#version 460 core
-
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec4 color;
-
-void main()
-{
-    
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,5 @@
-#include <spdlog/spdlog.h>
-
-#include "app.h"
 
 int main()
 {
-#ifdef _DEBUG
-	spdlog::set_level(spdlog::level::trace);
-#else
-	spdlog::set_level(spdlog::level::info);
-#endif
-	app app;
-	app.run();
 	return 0;
 }
