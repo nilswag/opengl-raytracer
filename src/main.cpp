a5804aa685baac7d8941b114f0bdeb85b8b696f9
nilswag <nilswagemaker05@icloud.com>
2026-05-13 15:01:25 +0200

    Started over again since vulkan was too hard

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
-#include <stdexcept>
-#include <iostream>
 #include <print>
-#include "app.hpp"
 
 int main()
 {
-	try
-	{
-		App app(800, 800, "Vulkan");
-		app.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::println(std::cerr, "Error: {}", e.what());
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
+	std::println("Hello World!");
+	return 0;
 }
