f75df7ed1b6fb4054f914d8e8bc528a3947b2d46
nilswag <nilswagemaker05@icloud.com>
2026-05-13 22:33:09 +0200

    Added main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
+#include <glad/gl.h>
 #include "gfx/window.h"
 
 int main()
 {
 	Window window(800, 800, "OpenGL");
+
+	while (!window.shouldClose())
+	{
+		window.pollEvents();
+
+		float color[3] = { 0 };
+		glClearNamedFramebufferfv(0, GL_COLOR, 0, color);
+		window.swapBuffers();
+	}
 
 	return 0;
 }
